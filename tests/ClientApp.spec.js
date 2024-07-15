dbeca4cca951b821cff4c1a5d5860e1c621a6d3a
DavGuzman <david.guzmanroldan@amaris.com>
2024-07-15 14:35:01 +0200

    🚧 Continue e2e test
    Explore a table for do a click

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -41,6 +41,14 @@ test.only('E2E test for e-commerce', async({page}) => {
     const cartBtn = page.locator('[routerlink*="cart"]')
     const nameInCart = page.locator('h3:has-text("ADIDAS ORIGINAL")')
     const cartSection = page.locator('div li')
+    const checkoutBtn = page.locator('text=Checkout')
+    const userMailInCheckOut = page.locator('.user__name > label')
+    const placeOrderBtn = page.locator('.action__submit')
+    const messageThankForBuy = page.locator('.hero-primary')
+    const idPedido = page.locator('.em-spacer-1 > .ng-star-inserted')
+    const ordersBtn = page.locator('[routerlink*="myorders"]').first()
+    const table = page.locator('.table-responsive')
+    const idElementsTable = page.locator('.ng-star-inserted > th')
 
     await userName.fill(userMail)
     await password.fill(userPassword)
@@ -77,8 +85,65 @@ test.only('E2E test for e-commerce', async({page}) => {
     console.log(`Comprobando productos en cesta: ${productTitleInCart}`)
     const bool = await nameInCart.isVisible()
     expect(bool).toBeTruthy()
+    await checkoutBtn.click()
 
+    //! Vamos a coger un dropdown que cuando introduces alguna letra te salen sugerencias.
+    const countryDropdown = page.locator('.form-group > input')
+    //! Al mandar este metodo no escribe letra por letra sino que pega el texto por lo tanto no saldra el menu de sugerencias que necesitamos para seleccionar
+    // await countryDropdown.fill('ind')
+    //! Con este metodo si que funciona ya que introduce letra por letra
+    await countryDropdown.pressSequentially('ind')
+    //! Con esto estamos esperando que aparezcan los 3 botones pero que solo se fije en esos 3 de dentro del desplegable y no de los botones de fuera
+    const dropdown = page.locator('.ta-results')
+    await dropdown.waitFor()
+    const optionsCount = await dropdown.locator('button').count()
 
-    // await page.pause()
+    for(let i =0; i<optionsCount; i++){
+
+        const options = dropdown.locator('button').nth(i)
+        const text = await options.textContent()
+        if (text.trim() === "India"){//|| text.includes('India'))
+            await options.click()
+            break
+        }
+    }
+
+    await expect(userMailInCheckOut).toHaveText(userMail)
+    await placeOrderBtn.click()
+
+    console.log(await messageThankForBuy.textContent())
+    await expect(messageThankForBuy).toHaveText(' Thankyou for the order. ')
+    
+    let idProcesada = await idPedido.textContent()
+    idProcesada = idProcesada.replace(/[|]/g, '').trim();
+    console.log(`ID procesada: ${idProcesada}`);
+    await ordersBtn.click()
+
+    await table.waitFor()
+
+    const rowsSelector = '.table-responsive tr'; 
+    await page.waitForSelector(rowsSelector, { state: 'visible' });
+
+    const rows = table.locator(rowsSelector);
+    const rowCount = await rows.count();
+    console.log(`Número de filas en la tabla: ${rowCount}`);
+    
+    for(let a = 0; a < rowCount; a++) {
+
+        const idElementsTableProcessed = await rows.nth(a).locator('.ng-star-inserted > th').textContent();
+        console.log(`ID en la tabla: ${idElementsTableProcessed}`);
+
+        if(idElementsTableProcessed.trim() === idProcesada) {
+
+            console.log(`Las id's encontradas son ${idElementsTableProcessed} ya ${idProcesada}`)
+            const viewBtn = rows.nth(a).locator('button:text("View")')
+            await viewBtn.click()
+            console.log(`Clicked view button for ID: ${idProcesada}`);
+            break
+        }
+
+    }
+
+    await page.pause()
 
 });
